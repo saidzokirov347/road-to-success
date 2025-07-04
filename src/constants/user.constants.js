4680fa36a14564b8ec603e39887c43ab21326f45
InomjonDev <inomjonismanaliev@gmail.com>
2025-07-05 01:09:31 +0500

    exp level

--- src/constants/user.constants.js
+++ src/constants/user.constants.js
@@ -1,6 +1,8 @@
-export const LEVEL_THRESHOLDS = [0, 1000, 2000, 3000, 4000, 5000]
+export const LEVEL_THRESHOLDS = [
+	0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
+]
 
-export const MAX_LEVEL = 5
+export const MAX_LEVEL = 10
 
 export const getDefaultProfileData = currentUser => ({
 	bio: '',
