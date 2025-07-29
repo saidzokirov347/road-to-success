f80ae8c27d5c33322ad912f8d031f88a3c95534f
InomjonDev <inomjonismanaliev@gmail.com>
2025-07-29 14:30:58 +0500

    button

--- src/components/writing-task-cart/WritingTaskCard.jsx
+++ src/components/writing-task-cart/WritingTaskCard.jsx
@@ -2,19 +2,30 @@ import './WritingTaskCard.css'
 
 export function WritingTaskCard({ task }) {
 	return (
-		<div className='writing-task-card'>
-			<div className='writing-task-card-img'>
-				<img src={task.imageUrl} alt='Writing Task' />
-			</div>
-			<div className='card-content'>
-				<span className='task-tag'>Task 1</span>
-				<h2 className='task-question'>{task.question}</h2>
-				<div className='task-meta'>
-					<span>By Admin</span>
-					<span>
-						{new Date(task.createdAt?.seconds * 1000).toLocaleDateString()}
-					</span>
+		<div
+			className={`writing-task-card ${
+				task.type === 'task-2' && !task.imageUrl
+					? 'writing-task-card-type-2'
+					: ''
+			}`}
+		>
+			{task.type === 'task-1' && task.imageUrl && (
+				<div className='writing-task-card-img'>
+					<img src={task.imageUrl} alt='Writing Task' />
 				</div>
+			)}
+			<div className='card-content'>
+				<span className='task-tag'>
+					{task.type === 'task-1'
+						? 'Type 1'
+						: task.type === 'task-2'
+						? 'Type 2'
+						: 'Unknown'}
+				</span>
+				<h2 className='task-question'>{task.question}</h2>
+				<a href={`/writing/${task.id}`} className='writing-task-link'>
+					View Task
+				</a>
 			</div>
 		</div>
 	)
